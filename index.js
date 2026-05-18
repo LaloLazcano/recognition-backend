a920504128a6434fe75fdd195d9cbb16b3224dda
LaloLazcano <121916343+LaloLazcano@users.noreply.github.com>
2026-05-18 16:51:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+const express = require("express");
+const cors = require("cors");
 
-console.log("API_KEY ENV:", process.env.API_KEY);
-console.log("API_KEY HEADER:", req.headers["x-api-key"]);
+const recognitionsRoutes = require("./routes/recognitions");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 🔐 SEGURIDAD API KEY
+app.use((req, res, next) => {
+  if (req.path === "/health") return next();
+
+  const apiKey = req.headers["x-api-key"];
+
+  if (apiKey !== process.env.API_KEY) {
+    return res.status(401).json({ error: "No autorizado" });
+  }
+
+  next();
+});
+
+app.use("/recognitions", recognitionsRoutes);
+
+app.get("/", (req, res) => {
+  res.send("VERSION NUEVA");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+``
